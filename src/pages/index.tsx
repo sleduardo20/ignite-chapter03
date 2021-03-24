e2b70ea2d2baf98a9e910bc138247feca4e711ee
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-24 09:45:18 -0300

    (feat)🧠: create Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,29 @@
+import Head from 'next/head';
+
+import styles from './home.module.scss';
+
 const Home = () => {
-  return <h1>World hold on</h1>;
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👋 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span> for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/img/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 };
 
 export default Home;
