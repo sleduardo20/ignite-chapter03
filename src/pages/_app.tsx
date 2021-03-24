edebdb2f3cd2c22c086f0793af2366760c0ea944
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-23 23:14:30 -0300

    🚀 initial commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,23 +1,19 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 
-import GlobalStyles from 'styles/GlobalStyles';
+import '../styles/global.scss';
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>React Avançado - Boilerplate</title>
-        <link rel="shortcut icon" href="/img/icon-512.png" />
-        <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <link rel="manifest" href="/manifest.json" />
         <meta
           name="description"
-          content="A simple project starter to work TypeScript, React,NextJS and Styled Components"
+          content="Bootcamp Ignite, chapter 03 aplicação da um app com Next, Primic, FaunaDB and Strapi"
         />
       </Head>
       <Component {...pageProps} />
-      <GlobalStyles />
     </>
   );
 }
