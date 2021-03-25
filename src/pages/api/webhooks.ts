bea597d7f70c5d0b42b8a9c28a592d21cba35502
sleduardo20 <sl.eduardo20@gmail.com>
2021-03-25 13:12:16 -0300

    (feat)🎧: listen events of stripe and save in faunadb

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,7 +22,11 @@ export const config = {
   },
 };
 
-const relevantEvents = new Set(['checkout.session.completed']);
+const relevantEvents = new Set([
+  'checkout.session.completed',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
@@ -46,6 +50,18 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (relevantEvents.has(type)) {
       try {
         switch (type) {
+          case 'customer.subscription.updated':
+          case 'customer.subscription.deleted':
+            const subscription = event.data.object as Stripe.Subscription;
+
+            await saveSubscription(
+              subscription.id,
+              subscription.customer.toString(),
+              false,
+            );
+
+            break;
+
           case 'checkout.session.completed':
             const checkoutSession = event.data
               .object as Stripe.Checkout.Session;
@@ -53,6 +69,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             await saveSubscription(
               checkoutSession.subscription?.toString(),
               checkoutSession.customer?.toString(),
+              true,
             );
 
             break;
